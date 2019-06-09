3fd993942a5a60307eba0a6bc15d66879fcb6c63
Antonio Fernandes <fernandes.anttonio@gmail.com>
2019-06-09 10:14:17 -0300

    Lista 8

--- LISTA_8.R
+++ LISTA_8.R
@@ -5,24 +5,24 @@
 
 # 5.5.1.1
 
-  # PACOTES
+  # Pacotes
 
 if(require(tidyverse) == F) install.packages('tidyverse'); require(tidyverse)
 if(require(rvest) == F) install.packages('rvest'); require(rvest)
 if(require(httr) == F) install.packages('httr'); require(httr)
 if(require(xml2) == F) install.packages('xml2'); require(xml2)
 
-  # LINK DO SITE
+  # link do site
 
 link <- "https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_do_Brasil_por_IDH"
 
-  # DADOS
+  # dados
 
 conteudo <- readLines(link, encoding = 'UTF-8') # Definindo como UTF-8 devido a acentuacao
 
 head(conteudo)
 
-  # IDENTIFICANDO PERNAMBUCO
+  # identificando pernambuco
 
 grep("Pernambuco", conteudo)  # A LINHA DO VETOR PERNAMBUCO É 784
 grep("Distrito Federal", conteudo) # IDENTIFICANDO A LINHA COM O NOME DA PRIMEIRA UF
@@ -31,6 +31,8 @@ conteudo[181 - 81] # ATE O VETOR DE NUMERO 109 VERIFICA-SE UFs
 indice <- 109 # COMECANDO COM O VETOR 109
 nomes_uf <- NULL 
 i <- 1
+
+  # coletando a informacao da uf
 
 while(indice < 1081){
   if(i == 1){
@@ -43,6 +45,8 @@ while(indice < 1081){
 }
 
 nomes_uf
+
+  # removendo caracteres especiais de uf
 
 nomes_uf <- gsub("[[:print:]]+\">", "", nomes_uf) 
 nomes_uf <- gsub("</a>", "", nomes_uf) 
@@ -65,34 +69,57 @@ pagina <- htmlParse(pagina)
 
 pagina <- xmlRoot(pagina)
 
+  # nodes da pagina
 
 nodes_link <- getNodeSet(pagina, '//*[@id="content"]')
 
+  # node do nome do parlamentar
+
 nome_parlamentar <- xpathSApply(pagina, '//*[@id="content"]/ul/li/a', xmlValue)
+
+  # node do site
 
 site <- xpathSApply(pagina, '//*[@id="content"]/ul/li/a', xmlGetAttr, name = "href")
 
+  # convertendo para dataframe
+
 dados <- data.frame(nome_parlamentar, site)
+
+  # salvando os dados
 
 save(dados, file = "dados_parlamentar.RDta")
 
 
 # 5.7.0.1
 
+  # carregando pacotes necessarios
+
 if(require(httr) == F) install.packages('httr'); require(httr)
 if(require(XML) == F) install.packages('XML'); require(XML)                                                         
 if(require(xml2) == F) install.packages('xml2'); require(xml2)
 
+  # paste link do site
+
 link <- paste0("http://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterDeputados")
 
+  # obtendo os dados
+
 response <- GET(link)
+
+  # convertendo para xml
 
 data <- xmlParse(response, encoding = "UTF-8")
 ls <- xmlToList(data)
 
+  # lapply para selecionar apenas a uf 'pe'
+
 x <- ls[lapply(ls, '[[',"uf")=='PE']
 
+  # identificando os nomes presentes na lista
+
 names(x$deputado)
+
+  # criando um vetor para cada parte da lista
 
 ideCadastro <- NULL
 condicao <- NULL 
@@ -105,6 +132,8 @@ sexo <- NULL
 uf <- NULL
 partido <- NULL 
 email <- NULL
+
+  # atribuindo as informações da lista para cada vetor criado
 
 
 for(i in 1:length(x)){
@@ -121,6 +150,7 @@ for(i in 1:length(x)){
   email[i] <- x[[i]]$email
 }
 
+  # transformando em data.frame
 
 bd <- data.frame(ideCadastro, condicao, matricula, idParlamentar, 
                  nome, nomeParlamentar, urlFoto, sexo, uf, partido, email)
@@ -151,13 +181,23 @@ dep_df <- as.data.frame(dep_text_json)
 
       # Segunda alternativa
 
+# pacotes necessarios
+
 if(require(httr) == F) install.packages('httr'); require(httr)
 if(require(XML) == F) install.packages('XML'); require(XML)                                                         
 if(require(xml2) == F) install.packages('xml2'); require(xml2)
+if(require(plyr) == F) install.packages('plyr'); require(plyr)
+
+# criando vetor com as legislaturas para o loop
 
 jump <- seq(41, 55, by = 1)
 
+# site 
+
 site <- paste0("https://www.camara.leg.br/internet/deputado/DepNovos_Lista.asp?Legislatura=",jump,"&Partido=QQ&SX=QQ&Todos=None&UF=QQ&condic=QQ&forma=lista&nome=&ordem=nome&origem=")
+
+
+# usando lapply para coletar o nome dos parlamentares de todas as legislaturas
 
 nome <- lapply(site, function(i) {
   pagina <- readLines(i)
@@ -172,6 +212,9 @@ nome <- lapply(site, function(i) {
   nome_parlamentar <- xpathSApply(pagina, '//*[@id="content"]/ul/li/a', xmlValue)
 })  
 
+# usando lapply para coletar o site dos parlamentares de todas as legislaturas
+
+
 site <- lapply(site, function(i) {
   pagina <- readLines(i)
   
@@ -185,3 +228,27 @@ site <- lapply(site, function(i) {
   site <- xpathSApply(pagina, '//*[@id="content"]/ul/li/a', xmlGetAttr, name = "href")
 })  
 
+# atribuindo o numero da respectiva legislatura para cada elemento da lista em nome 
+
+names(nome) <- jump
+nome <- ldply(nome, data.frame)
+
+# unlist site 
+
+site <- unlist(site)
+site <- as.data.frame(site)
+
+# juntando os dois dataframes e arrumando os nomes das variaveis
+ 
+ deputados <- cbind(nome, site)
+ 
+ deputados$leg <- deputados$.id
+ deputados$nome <- deputados$X..i..
+    
+ deputados$.id <- NULL
+ deputados$X..i.. <- NULL
+ 
+ deputados$nome <- gsub('/', '', deputados$nome)
+
+ 
+  
